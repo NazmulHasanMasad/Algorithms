56e9eb33507eefd09a15bd36b01f53e1f692d13b
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2024-08-31 13:08:10 +0600

    left shift operator

---
+++ left_shift_operator.cpp
@@ -0,0 +1,17 @@
+// C++ Program to demonstrate use
+// of left shift  operator
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+
+    unsigned char a = 2, b = 2;
+
+    cout << (1 << a) << endl;
+    cout << (5 | 4) << endl;
+
+    // cout << "b<<1 = " << (b << 1) << endl;
+    return 0;
+}
